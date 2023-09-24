81465dbad9e6e5299859b5e68c1c38e171b8965f
DevAdeelAhmad <devadeelahmad@gmail.com>
2023-09-25 02:01:36 +0500

    Status Bar set to light

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light"/>
+      <StatusBar style="light" />
       <CategoriesScreen />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
 });
